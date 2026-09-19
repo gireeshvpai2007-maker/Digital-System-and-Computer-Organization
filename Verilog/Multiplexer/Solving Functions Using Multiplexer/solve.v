65d631d2b46d3e5d88d41a8a2cf564a7c879cbe2
gireeshvpai2007-maker <gireeshvpai2007@gmail.com>
2026-09-19 21:45:50 +0530

    Added Solution to solve f=ab'c +a'b+bc' using two 2:1 mux and an inverter

---
+++ Verilog/Multiplexer/Solving Functions Using Multiplexer/solve.v
@@ -0,0 +1,30 @@
+/*Solve this function using two 2:1 mux and a inverter ,
+f(a,b,c)=∑(2,3,5,6) =ab'c+a'b+bc' */
+module solve(
+    input a,
+    input b,
+    input c,
+    output f
+);
+wire g;
+mux2to1 mux1(
+    .s(b),
+    .d({c, ~c}),
+    .y(g)
+);
+mux2to1 mux2(
+    .s(a),
+    .d({g, b}),
+    .y(f)
+);
+endmodule
+module mux2to1(
+    reg s,
+    reg [1:0] d,
+    wire y
+);
+if(s==0)
+    y=d[0];
+else
+    y=d[1];
+endmodule
